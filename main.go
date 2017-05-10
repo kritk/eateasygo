c0edf169020a710412af13bc43f8d61c58eb4a30
kritk <kritmairuu@gmail.com>
2017-05-10 15:35:07 +0700

    add create

--- main.go
+++ main.go
@@ -7,8 +7,14 @@ import (
 
 func init() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/create",createhandler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "Hello, world!")
+}
+
+func createhandler(w http.ResponseWriter, r *http.Request){
+	f(r.Context())
+	fmt.Fprint(w,"Save successful")
 }
