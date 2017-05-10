176aaca26052ec883a58da73accb3feab35deeae
kritk <kritmairuu@gmail.com>
2017-05-10 15:28:30 +0700

    test datastore

---
+++ employee.go
@@ -0,0 +1,31 @@
+package hello
+import (
+        "time"
+
+        "golang.org/x/net/context"
+
+        "google.golang.org/appengine/datastore"
+)
+
+type Employee struct {
+        FirstName          string
+        LastName           string
+        HireDate           time.Time
+        AttendedHRTraining bool
+}
+
+func f(ctx context.Context) {
+        // ...
+        employee := &Employee{
+                FirstName: "Antonio",
+                LastName:  "Salieri",
+                HireDate:  time.Now(),
+        }
+        employee.AttendedHRTraining = true
+
+        key := datastore.NewIncompleteKey(ctx, "Employee", nil)
+        if _, err := datastore.Put(ctx, key, employee); err != nil {
+                // Handle err
+        }
+        // ...
+}
